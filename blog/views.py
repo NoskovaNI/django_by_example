e20182b908e3d300ab54f5f5da7f52292295068a
noskova_ni <noskova_ni@inbox.ru>
2021-08-06 21:51:31 +0300

    first chapter

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 # The logic of your application goes here; each view receives an
 # HTTP request, processes it, and returns a response.
 
 
-# display list of posts
+# display list of posts with pagination
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 post on each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page isn't an int, deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range, deliver last page
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'page': page,
+                   'posts': posts})
 
 
 # display detailed information of post
@@ -23,3 +37,11 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {"post": post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
